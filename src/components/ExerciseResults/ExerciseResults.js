872ab592c6a74439b6b5513c7e9adb8a07d7c61a
tuncakin <tuncak30@gmail.com>
2020-10-11 18:39:09 +0300

    v0.0.1 finished

--- src/components/ExerciseResults/ExerciseResults.js
+++ src/components/ExerciseResults/ExerciseResults.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './ExerciseResults.scss';
 
 export default function ExerciseResults(props){
+    function addDefaultSrc(ev){
+        ev.target.src = document.location.origin + '/img/workouts/other.png';
+
+    }
 
     return(
         <div className="exercise-results mt-1 mb-2">
             <div className="exercise-thumbs">
-
+                <img onError={addDefaultSrc}className="img-fluid exercise-icons" src={document.location.origin + '/img/workouts/' + props.data.name.replace(/ /g,'') + '.png'} alt={props.data.name}/>
             </div>
             <div className="exercise-rows exercise-name">
                 {props.data.name.charAt(0).toUpperCase() + props.data.name.slice(1)}
